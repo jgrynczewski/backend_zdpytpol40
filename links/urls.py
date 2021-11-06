1feef3f22859283a4a09dd0923dc663ed699c2bf
jgrynczewski <Tel^)%322))$>
2021-11-06 14:27:52 +0100

    Przestrzenie nazw mapowan

--- links/urls.py
+++ links/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from links import views
 
+app_name = 'links'
+
 urlpatterns = [
     path('pierwsza/', views.first, name="first"),
     path('druga/', views.second, name="second"),
